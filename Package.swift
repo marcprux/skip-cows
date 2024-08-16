8b61ae74590c411306f309df6b2ed38b94a5ca7e
Helge Heß <devteam@zeezide.de>
2024-08-16 16:37:14 +0200

    Trim manifest and add skip
    We'll see whether that works.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,18 @@
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-  name: "cows",
-	
-	exclude: [
-		"cows.txt",
-		"cows.xcodeproj",
-		"GNUmakefile",
-		"LICENSE",
-		"README.md",
-		"xcconfig"
-	]
+  name         : "cows",
+  products     : [
+    .library   (name: "cows", targets: [ "cows" ]),
+    .executable(name: "vaca", targets: [ "vaca" ])
+  ],
+  dependencies: [
+    .package(url: "https://source.skip.tools/skip.git", from: "1.0.4")
+  ],
+  targets      : [ 
+    .target(name: "cows",
+            plugins: [ .plugin(name: "skipstone", package: "skip") ]),
+    .target(name: "vaca", dependencies: [ "cows" ])
+  ]
 )
--- Package@swift-4.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version:4.0
-import PackageDescription
-
-let package = Package(
-  name         : "cows",
-  products     : [ .library(name: "cows", targets: ["cows"]) ],
-  dependencies : [],
-  targets      : [ .target(name:"cows") ]
-)
